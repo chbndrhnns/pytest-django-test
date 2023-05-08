5cb6348cb46c5498af7c941ca129e3dfb58d2efc
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-05-08 16:43:25 +0200

    refactor: Use view classes

--- myfirst/polls/views.py
+++ myfirst/polls/views.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def index(request):
