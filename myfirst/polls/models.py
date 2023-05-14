318bd14412e7c46a0b59f521a893f95be63bbb51
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-05-14 11:47:24 +0200

    fix: was_published_recently

--- myfirst/polls/models.py
+++ myfirst/polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -9,7 +11,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) < self.pub_date <= now
 
     def __str__(self):
         return self.question_text
