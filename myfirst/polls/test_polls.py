7a5e79f4836242fec6feed813d9d47c946586f09
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-05-14 11:49:19 +0200

    test: More tests for was_published_recently

--- myfirst/polls/test_polls.py
+++ myfirst/polls/test_polls.py
@@ -7,10 +7,18 @@ from .models import Question
 
 
 def test_was_published_recently_with_future_question():
-    """
-    was_published_recently() returns False for questions whose pub_date
-    is in the future.
-    """
     time = timezone.now() + datetime.timedelta(days=30)
     future_question = Question(pub_date=time)
     assert not future_question.was_published_recently()
+
+
+def test_was_published_recently_with_old_question():
+    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+    future_question = Question(pub_date=time)
+    assert not future_question.was_published_recently()
+
+
+def test_was_published_recently_with_recent_question():
+    time = timezone.now() - datetime.timedelta(minutes=1)
+    future_question = Question(pub_date=time)
+    assert future_question.was_published_recently()
