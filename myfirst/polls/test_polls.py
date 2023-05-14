34ad99972cacf51cd94c9f7553338710da2fb4b5
chbndrhnns <chbndrhnns@users.noreply.github.com>
2023-05-14 12:05:32 +0200

    refactor: Move tests into class

--- myfirst/polls/test_polls.py
+++ myfirst/polls/test_polls.py
@@ -1,24 +1,22 @@
 import datetime
 
-from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
 
 
-def test_was_published_recently_with_future_question():
-    time = timezone.now() + datetime.timedelta(days=30)
-    future_question = Question(pub_date=time)
-    assert not future_question.was_published_recently()
+class TestPublished:
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        assert not future_question.was_published_recently()
 
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        future_question = Question(pub_date=time)
+        assert not future_question.was_published_recently()
 
-def test_was_published_recently_with_old_question():
-    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-    future_question = Question(pub_date=time)
-    assert not future_question.was_published_recently()
-
-
-def test_was_published_recently_with_recent_question():
-    time = timezone.now() - datetime.timedelta(minutes=1)
-    future_question = Question(pub_date=time)
-    assert future_question.was_published_recently()
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(minutes=1)
+        future_question = Question(pub_date=time)
+        assert future_question.was_published_recently()
